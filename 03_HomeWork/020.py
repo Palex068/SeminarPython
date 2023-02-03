39ffeb8658962fddb7f2c8ac39c4e38da310ad62
Alexandr Pakhomov <palex068@gmail.com>
2023-02-03 11:56:14 +0300

    Scrabble - ценность слова

--- 03_HomeWork/020.py
+++ 03_HomeWork/020.py
@@ -25,3 +25,41 @@
 Вывод:
 12
 """
+import os
+os.system('cls')
+
+
+def letter_cost(ch):
+    
+    ch = ch.upper()
+    if ch in " ":
+        n = 0
+    elif ch in "AEIOULNSTRАВЕИНОРСТ":
+        n = 1
+    elif ch in "DGДКЛМПУ":
+        n = 2
+    elif ch in "BCMPБГЁЬЯ":
+        n = 3
+    elif ch in "FHVWYЙЫ":
+        n = 4
+    elif ch in "KЖЗХЦЧ":
+        n = 5
+    elif ch in "JXШЭЮ":
+        n = 8
+    elif ch in "QZФЩЪ":
+        n = 10
+    return n
+
+def word_counter(word):
+    word_count = 0
+    word = word.upper()
+    for i in word:
+        l_c = letter_cost(i)
+        word_count += l_c
+        print(f"{i} = {l_c}")
+    return word_count
+
+word = input(f"Введите слово: ")
+print()
+
+print(f"\n{word_counter(word)}")
