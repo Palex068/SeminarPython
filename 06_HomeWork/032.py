60d2d1719a78eac1c16631c0ce3a39a90ea2b4b8
Alexandr Pakhomov <palex068@gmail.com>
2023-02-17 13:44:14 +0300

    Определить индексы элементов списка значения которых принадлежат заданному диапазону

--- 06_HomeWork/032.py
+++ 06_HomeWork/032.py
@@ -10,11 +10,5 @@
 # [1, 9, 13, 14, 19]
 """
 N = list(int(i) for i in input().split())
-n_min, n_max = int(input()), int(input())
-# print(N)
-# result = list()
-# for i in range(len(N)):
-#     if N[i] in range(n_min, n_max + 1):
-#         result.append(i)
-# print(result)
+n_min, n_max = (int(input()) for _ in 'ab')
 print(list(i for i in range(len(N)) if N[i] in range(n_min, n_max + 1)))
